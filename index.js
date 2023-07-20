33216c727ab9c42cb3e75c8aa09f34a92f8d4d53
Vincent Sebastian <vincent.sbst@gmail.com>
2023-07-20 22:52:05 +0700

    UPDATE : adding base routes and filename typo

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import errorHandler from "./src/middleware/error.handler.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors({ exposedHeaders : "Authorization" }))
+
+import AuthRouters from "./src/controllers/authentication/routers.js"
+import ProductRouters from "./src/controllers/product/routers.js"
+import ReportRouters from "./src/controllers/report/routers.js"
+import TransactionRouters from "./src/controllers/transaction/routers.js"
+
+app.use("/api/auth", AuthRouters)
+app.use("/api/product", ProductRouters)
+app.use("/api/report", ReportRouters)
+app.use("/api/transaction", TransactionRouters)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
